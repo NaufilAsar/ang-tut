15c3a0b9e74e1bbc21107bfbff4e8dd63f82ce0c
Naufil <naufilasar9028@gmail.com>
2022-10-31 23:14:58 +0530

    Fixed Categories fetching

--- src/app/home-page/home-page.component.ts
+++ src/app/home-page/home-page.component.ts
@@ -40,19 +40,48 @@ export class HomePageComponent implements OnInit {
   }
   ngOnInit(): void {
     this.developmentAutoClick();
+    this.resolveIfCategoryFound();
+  }
+
+  resolveIfCategoryFound(): void {
     this.activatedRoute.queryParams.subscribe((params: Params) => {
       if (Object.keys(params).length === 0) {
         console.log('no params found');
       } else {
-        console.log(params['category']);
+        this.productName = 'Category ' + params['category'];
         // reslove params
         let search_bar = <HTMLInputElement>(
           document.getElementById('search_box')
         );
         if (search_bar) {
-          search_bar.value = this.productName = params['category'];
+          search_bar.value = params['category'];
           this.usingParams = true;
-          this.onClickSearch();
+          let categoryArray: any = [];
+          if (params['category'] === 'Computer') {
+            categoryArray = [
+              'Computer',
+              'Mobile',
+              'Keyboard',
+              'Powerbank',
+              'Charger',
+            ];
+          } else if (params['category'] === 'Fashion') {
+            categoryArray = ['Mens Clothes', 'Womens Clothes'];
+          } else if (params['category'] === 'Sports') {
+            categoryArray = [
+              'Cricket Bat',
+              'Cricket Ball',
+              'Football',
+              'Tennis Racket',
+            ];
+          } else if (params['category'] === 'Appliances') {
+            categoryArray = ['Washing Machine', 'AC', 'Chimney', 'Television'];
+          } else if (params['category'] === 'Health') {
+            categoryArray = ['Protein Powder', 'Dumbells', 'Sportswear'];
+          }
+          for (let category of categoryArray) {
+            this.fetchProducts(category, true);
+          }
         }
       }
     });
@@ -69,15 +98,24 @@ export class HomePageComponent implements OnInit {
     this.displayLoadingAnimation = true; // display loading animation
     // Access API and display the products
     this.fetchProducts(this.search_product.value);
+    this.productName != this.search_product.value;
   }
 
-  fetchProducts(item: string | null) {
-    this.productName != item;
+  fetchProducts(item: string | null, ifCategory: Boolean = false) {
     let url = this.apiUrl + item?.replace(' ', '_');
     this.api.getProducts(url).subscribe({
       next: (products) => {
-        this.results = [];
-        this.results = products;
+        if (ifCategory) this.results = products;
+        else this.results.push(products);
+        this.results = this.results.filter((x: any) => {
+          return (
+            x.imgs != undefined &&
+            x.titles != undefined &&
+            x.site != undefined &&
+            x.prices != undefined &&
+            x.urls != undefined
+          );
+        });
         this.results = this.shuffleArray(this.results);
         console.log(this.results);
         this.resultsLoaded = true;
@@ -95,6 +133,7 @@ export class HomePageComponent implements OnInit {
 
   onClickViewPriceHistory(data: Object, suggestions: Object[]): void {
     console.log(data);
+
     // save product to local storage
     localStorage.setItem('product', JSON.stringify(data));
     let indexes = this.results[Math.floor(Math.random() * this.results.length)];
